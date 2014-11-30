9c83f79881e167910f3d810dce02dce91acbbbe2
Brooks Swinnerton <bswinnerton@gmail.com>
2014-11-30 18:37:18 -0500

    Updating spacing of keyboard layout

--- keyboard/hhkb/keymap_brooks.c
+++ keyboard/hhkb/keymap_brooks.c
@@ -61,8 +61,8 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      *       `-------------------------------------------'
      */
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS,GRV, \
-           TAB, Q,   MS_U,   E,   R,   T,   Y,   U,   I,   O,   P,LBRC,RBRC,BSPC,     \
-           LCTL,MS_L,MS_D,MS_R,   F,   G,   H,   J,   K,   L,SCLN,QUOT,ENT,           \
+           TAB, Q,   MS_U,E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,        \
+           LCTL,MS_L,MS_D,MS_R,F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,           \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,           \
                 LALT,LGUI,          BTN1,               RGUI,FN1),
 };
